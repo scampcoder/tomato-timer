f5fe28d7658d385c317e4a51f3335f6d09bb6aaf
Victoria Kee <victoriakee95@gmail.com>
2020-05-18 15:20:05 -0500

    First try at timer

--- js/tomato-timer.js
+++ js/tomato-timer.js
@@ -3,7 +3,21 @@ const minibreak = document.getElementById('mini');
 const longbreak = document.getElementById('long');
 
 function pomodoro(mins){
-  
+  let minutes = mins;
+  let seconds = mins * 60;
+  let countdown = minutes + ":" + seconds;
+  setInterval(function() {
+    document.getElementById('timer').innerHTML = countdown;
+    seconds--;
+    if (seconds < 0) {
+      minutes--;
+
+    }
+    if(minutes == 0 && seconds == 0){
+      alert("Timer Done!");
+    }
+  }, 1000);
+
 };
 
 work.addEventListener('click', pomodoro(25));
